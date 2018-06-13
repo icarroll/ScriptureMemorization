a90a055f5d55b2382c554aacd0349b51c47d953d
Cynthia <cynthia2@byui.edu>
2018-06-12 22:02:44 -0600

    Add unit tests and scripture class

--- app/src/test/java/cs246/scripturememorization/StoreDataUnitTest.java
+++ app/src/test/java/cs246/scripturememorization/StoreDataUnitTest.java
@@ -2,6 +2,7 @@ package cs246.scripturememorization;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.util.Date;
 
 import static org.junit.Assert.*;
@@ -10,11 +11,20 @@ import static org.junit.Assert.*;
 public class StoreDataUnitTest {
 
     @Test
+    public void writeDataTest() {
+        String filename = "numbers.txt";
+        File file = new File(filename);
+        assertTrue(file.canRead());
+    }
+
+    @Test
     public void readStoredData(){
         //calls "readData" method to get stored information
         // - converts JSON to Java object
 
         // testScripture = readData();
+
+        Scripture testScripture = new Scripture();
 
         assertEquals("Book of Mormon",testScripture.volume);
         assertEquals("1 Nephi", testScripture.book);
