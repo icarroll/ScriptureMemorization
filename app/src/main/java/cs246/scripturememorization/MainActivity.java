b3f33137e898d86a6d7a0ff27b850f97d5031bac
Aaron Eiche <aaron@aaroneiche.com>
2018-06-14 22:04:15 -0700

    Adds test button and method for sending scripture data to Recitation Activity. Adds Intent and handlers to retireve data from Intent.

--- app/src/main/java/cs246/scripturememorization/MainActivity.java
+++ app/src/main/java/cs246/scripturememorization/MainActivity.java
@@ -1,10 +1,15 @@
 package cs246.scripturememorization;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     //It works well!!
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public static final String REFERENCE = "";
+    public static final String VERSE = "";
+
+
+    public void sendScripture(View view) {
+        //just a temporary method for testing the Intent to opening the recitation view
+        //send the text to match, and the verse reference.
+        Log.d("TEMP", "Sending reference and verse");
+
+        String reference = "2 Nephi 2:24";
+        String verse = "But behold, all things have been done in the wisdom of him who knoweth all things.";
+
+        Intent reciteIntent = new Intent(this,ReciteActivity.class);
+        reciteIntent.putExtra(REFERENCE,reference);
+        reciteIntent.putExtra(VERSE,verse);
+
+        startActivity(reciteIntent);
+    }
     //commenting Look at my comment a thing.
 }
--- app/src/main/java/cs246/scripturememorization/ReciteActivity.java
+++ app/src/main/java/cs246/scripturememorization/ReciteActivity.java
@@ -1,7 +1,9 @@
 package cs246.scripturememorization;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class ReciteActivity extends AppCompatActivity {
 
@@ -9,6 +11,18 @@ public class ReciteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech);
+
+        Intent scripture = getIntent();
+
+        //
+        String ref = scripture.getStringExtra(MainActivity.REFERENCE);
+        String verse = scripture.getStringExtra(MainActivity.VERSE);
+
+        //Set the textView to the reference.
+        TextView thisReference = findViewById(R.id.scriptureReference);
+        thisReference.setText(verse);
     }
+
+
 
 }
