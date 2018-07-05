421ed20fce925ed7e81197dd5c56937c47f70630
haydendsimmons <haydendsimmons@gmail.com>
2018-07-05 00:54:27 -0400

    These are the files that correspond the new activity layouts from a little bit ago. I switched from one activity to one activity each for volumes, books, chapters, and verses. Not pushing all of their logic yet, just a placeholder while I make sure they all will work together.

---
+++ app/src/main/java/cs246/scripturememorization/GetChapterActivity.java
@@ -0,0 +1,13 @@
+package cs246.scripturememorization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetChapterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_chapter);
+    }
+}
